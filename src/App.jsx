95ce88df7109f5ab742b08bce9e0ce46123042e5
Adélie Saulé <adelie.saule@etu.minesparis.psl.eu>
2024-02-16 16:46:34 +0100

    gala2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
   <div>
       <p class="text-4xl text-green-700 text-center py-20">Le Grand Gala commence  à 19h30 (je cherche à rendre cette page utile au moins)</p>
-      <img src="">https://eventopediacdn.azureedge.net/content/GL-8-185.jpg</img>
+      <img> src="https://eventopediacdn.azureedge.net/content/GL-8-185.jpg"</img>
        
   </div>
   );
