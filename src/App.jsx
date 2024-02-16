13faf08b9329dcb8047a3383f76af623a8c1c6cc
Adélie Saulé <adelie.saule@etu.minesparis.psl.eu>
2024-02-16 16:40:02 +0100

    texte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
   <div>
-      <p class="text-4xl text-green-700 text-center py-20">Testing a push  2</p>
+      <p class="text-4xl text-green-700 text-center py-20">Le Grand Gala commence  à 19h30 (je cherche à rendre cette page utile au moins)</p>
   </div>
   );
 }
