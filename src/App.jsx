10d3cbaf3e4f4059cc78802069e77f294389d43e
Adélie Saulé <adelie.saule@etu.minesparis.psl.eu>
2024-02-16 16:30:22 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
   <div>
-      <p class="text-4xl text-green-700 text-center py-20">Helloooo Bro!</p>
+      <p class="text-4xl text-green-700 text-center py-20">Testing a push</p>
   </div>
   );
 }
